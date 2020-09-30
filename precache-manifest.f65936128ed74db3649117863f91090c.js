a3f0ae93243482dad70b59d87871123c4a9cc8cd
urbole <yurchenko.b.l@gmail.com>
2020-10-01 00:11:11 +0300

    Updates

---
+++ precache-manifest.f65936128ed74db3649117863f91090c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb273f5002c57db2c1f35367dcf7e233",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "3b432d5a053422e3601c",
+    "url": "/first-app/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "5f7ce9a84d236581cfd1",
+    "url": "/first-app/static/css/main.f47ace20.chunk.css"
+  },
+  {
+    "revision": "3b432d5a053422e3601c",
+    "url": "/first-app/static/js/2.886cd124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-app/static/js/2.886cd124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7ce9a84d236581cfd1",
+    "url": "/first-app/static/js/main.b06fae5c.chunk.js"
+  },
+  {
+    "revision": "0411795b630819b8d466",
+    "url": "/first-app/static/js/runtime-main.284ef3e7.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/first-app/static/media/GS_logo.0662d9de.svg"
+  },
+  {
+    "revision": "b236af76d3e12b9f2dc0a2d673375d86",
+    "url": "/first-app/static/media/Hero_bg.b236af76.jpg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/first-app/static/media/ITN_logo.2f311a05.svg"
+  },
+  {
+    "revision": "1d9ae9e91593976f92150a639bbdf321",
+    "url": "/first-app/static/media/energy.1d9ae9e9.jpg"
+  },
+  {
+    "revision": "3d66b58c57513e79022af1cbb2936e5f",
+    "url": "/first-app/static/media/industry.3d66b58c.jpg"
+  },
+  {
+    "revision": "17b056bde9ed2137fe8aec437de3fec4",
+    "url": "/first-app/static/media/power.17b056bd.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/first-app/static/media/shid_logo_1.341a487c.svg"
+  },
+  {
+    "revision": "3e6d941cbfa160cc45b27d1288d9d561",
+    "url": "/first-app/static/media/slide-1.3e6d941c.jpg"
+  },
+  {
+    "revision": "2d01e3239b6bf044acb33d0c494d140c",
+    "url": "/first-app/static/media/slide-2.2d01e323.jpg"
+  },
+  {
+    "revision": "2e8165b3ecf02d46bd2229aeeef76b9f",
+    "url": "/first-app/static/media/slide-3.2e8165b3.jpg"
+  },
+  {
+    "revision": "4bfc613740f98e03bb39a65ba436d672",
+    "url": "/first-app/static/media/slide-4.4bfc6137.jpg"
+  },
+  {
+    "revision": "b2936b7dcfaeb3423ea4a3123ef85f39",
+    "url": "/first-app/static/media/slide-5.b2936b7d.jpg"
+  },
+  {
+    "revision": "fb06d4dd85a3187e7d299e96bd5393b8",
+    "url": "/first-app/static/media/transport.fb06d4dd.jpg"
+  },
+  {
+    "revision": "19cb675508b5ffec4024e651a6e5ec0e",
+    "url": "/first-app/static/media/uaste-management.19cb6755.jpg"
+  },
+  {
+    "revision": "a3fe3aa17418ceb9f8e4c1961faf77e0",
+    "url": "/first-app/static/media/urban-planning.a3fe3aa1.jpg"
+  }
+]);
